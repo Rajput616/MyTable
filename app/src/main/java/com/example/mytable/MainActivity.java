a63f823f89348e248895981e573ca3be2c17cf85
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2021-03-18 23:27:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mytable/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.mytable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int VERY_SHORT = 500;
+    TextView textView;
+    ListView listView;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        listView = findViewById(R.id.listView);
+        seekBar = findViewById(R.id.seekBar);
+
+
+        seekBar.setMax(30);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser) {
+                    final Toast toast = Toast.makeText(MainActivity.this, "Computing Table of " + progress , Toast.LENGTH_SHORT);
+                    toast.show();
+
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            toast.cancel();
+                        }
+                    }, 500);
+                    populate(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void populate(int table){
+        ArrayList<String> mulTable = new ArrayList<>();
+        for(int i = 1; i <= 10; i++){
+            mulTable.add(table + " X " + i + " = " + table*i);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.
+                                            simple_list_item_1, mulTable);
+        listView.setAdapter(arrayAdapter);
+        textView.setText("Multiplication Table of " + table);
+    }
+}
